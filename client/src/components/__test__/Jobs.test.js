69789be615cc81e75ad0e85a0b66a6b71e11623c
Mian Aziz Ahmad <azizahmad6599@email.com>
2022-05-25 05:39:19 -0700

    React-hooks

--- client/src/components/__test__/Jobs.test.js
+++ client/src/components/__test__/Jobs.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Job from './../Job';
 import "@testing-library/jest-dom/extend-expect"
+// import { act } from 'react-dom/test-utils';
 import { fireEvent } from '@testing-library/react';
 import renderer from 'react-test-renderer';
+import { renderHook, act } from '@testing-library/react-hooks/dom'
 
 describe('Jobs Test Case { ', ()=>{
 
@@ -27,11 +29,15 @@ describe('Jobs Test Case { ', ()=>{
         jobLocation="Gulberg"
         job="Associate Developer"
         createdAt="HR"
-        status="Offered" onClick={fn}></Job>).toJSON();
-        const button = tree.root.getElementbyId('edit');
-        button.props.onClick()
+        status="Offered" onClick={fn}></Job>);
+        const btn = component.root.findByType('button');
+        act(btn.props.onClick);
+        // act(() => {
+        //     fireEvent.click(btn.props);
+        //   });
         // Verify callback is invoked
-        expect(fn.mock.calls.length).toBe(1);
-        expect(tree.root.getElementbyId('type').value).toBe("Clerk");
+        console.log(fn.mock.calls.length);
+        // expect(fn.mock.calls.length).toBe(1);
+        expect(component.root.getElementbyId('type').value).toBe("Clerk");
     });
 });
